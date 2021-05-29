e1e9fe4932ff4a863d4a44876b16870d93002f89
Nursamad <bagishbekow@gmail.com>
2021-05-29 17:52:26 +0600

    first commit

--- src/com/geektech/Main.java
+++ src/com/geektech/Main.java
@@ -1,22 +1,24 @@
 package com.geektech;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        String name = "Nursamad"; //string это переменная
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
         int age = generateRandomAge();
         int temp = 20;
         System.out.println(generateRandomAge());
         if (age > 20 && age < 45 && temp < 30 && temp > -20){
-            System.out.println("Можно идти гулять!");
+            System.out.println("Можно идти гулять! " + name);
         } else if (age < 20 && temp > 0 && temp < 28){
-            System.out.println("Можно гулять!");
+            System.out.println("Можно гулять! " + name);
         } else if (age > 45 && temp > -10 && temp < 25){
-            System.out.println("Можно гулять!");
+            System.out.println("Можно гулять! " + name);
         } else {
-            System.out.println("Оставайтесь дома!");
+            System.out.println("Оставайтесь дома!" + name);
         }
     }
 
@@ -26,8 +28,11 @@ public class Main {
         int age = random.nextInt(107);
 
         return age;
+
+
+    }
     }
 
 
-}
+
 
